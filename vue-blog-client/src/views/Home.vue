b98fadd7d69c6a5795d75da4059b17f45d78a581
liuyang19910220 <312111368@qq.com>
2017-11-21 23:47:04 +0800

    aaa

---
+++ vue-blog-client/src/views/Home.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <div class="layout-breadcrumb">
+          <Breadcrumb>
+              <Breadcrumb-item href="#/">首页</Breadcrumb-item>
+             
+          </Breadcrumb>
+      </div>
+  </div>
+</template>
